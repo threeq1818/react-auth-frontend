cb9660354756fe7b73a28e46a92f5865f62aff7e
Three Q <threeq1818@gmail.com>
2019-07-09 22:40:28 +0800

    create navbar

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
 import React, { Component } from 'react';
 import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
-import logo from './logo.svg';
-import './App.css';
+import Navbar from './components/Navbar';
 
 class App extends Component {
   render() {
